9e35c40f671373cf5675be6c906f719428bf8685
Johnnei <jtiesinga@gmail.com>
2023-11-28 09:59:25 +0700

    Add scala metals

--- nvim/lua/plugins/lsp/init.lua
+++ nvim/lua/plugins/lsp/init.lua
@@ -11,13 +11,20 @@ return {
 	},
 	{
 		"williamboman/mason-lspconfig.nvim",
+		opts = {
+			ensure_installed = {
+				"rust_analyzer"
+			},
+		},
 	},
 	{
 		"neovim/nvim-lspconfig",
 		config = function(self, opts)
 			local lspconfig = require("lspconfig")
 			local mason = require("mason")
+			local masonlsp = require("mason-lspconfig")
 			mason.setup()
+			masonlsp.setup()
 			lspconfig.rust_analyzer.setup({})
 		end
 	},
@@ -27,8 +34,17 @@ return {
 			ensure_installed = {
 				"lua",
 				"rust",
-				"scala",
 			},
 		},
+	},
+	{
+		"scalameta/nvim-metals",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		build = ":MetalsInstall",
+		config = function(self, metals_config)
+			require("metals").initialize_or_attach(metals_config)
+		end
 	},
 }
