be6c606653db1b2c0a715b47f0fecf00d90a3b41
Johnnei <jtiesinga@gmail.com>
2023-12-06 10:28:08 +0700

    Add telescope

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,6 +13,7 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 vim.g.mapleader = " " -- Make sure to set `mapleader` before lazy so your mappings are correct
+vim.g.maplocalleader = "\\"
 
 require("lazy").setup("plugins")
 
--- nvim/lua/plugins/editor.lua
+++ nvim/lua/plugins/editor.lua
@@ -1,11 +1,23 @@
 return {
+	-- Add File explorer
 	{
 		"nvim-neo-tree/neo-tree.nvim",
 		branch = "v3.x",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
+			"MunifTanjim/nui.nvim",
 		},
 	},
+	-- Add file search
+	{
+		"nvim-telescope/telescope.nvim",
+		branch = "0.1.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		keys = {
+			--			{ "<leader>ff", require("telescope.builtin")["files"]({}),                  desc = "Find Files (root dir)" },
+		}
+	}
 }
