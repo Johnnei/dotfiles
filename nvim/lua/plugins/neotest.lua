87cc1e41e95509be6e9af19c569f2b6e45b0db4a
Johnnei <jtiesinga@gmail.com>
2024-01-29 22:00:37 +0700

    Add neotest dap binding

--- nvim/lua/plugins/neotest.lua
+++ nvim/lua/plugins/neotest.lua
@@ -34,6 +34,7 @@ return {
 			{ '<leader>tt', function() require("neotest").run.run(vim.fn.expand("%")) end, desc = "Run tests in file" },
 			{ '<leader>tT', function() require("neotest").run.run(vim.loop.cwd()) end, desc = "Run tests in directory" },
 			{ '<leader>tr', function() require("neotest").run.run() end, desc = "Run nearest test" },
+			{ "<leader>td", function() require("neotest").run.run({ strategy = "dap" }) end, desc = "Debug Nearest" },
 			{ '<leader>ts', function() require("neotest").summary.toggle() end, desc = "Toggle summary" },
 			{ '<leader>to', function() require("neotest").output.open({ enter = true, auto_close = true}) end, desc = "Show output" },
 			{ '<leader>tO', function() require("neotest").output_panel.toggle() end, desc = "Show output panel" },
