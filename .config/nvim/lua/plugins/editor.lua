f8ccea119ee907a6b9542bc012b0b02f7d5d9627
Johnnei <jtiesinga@gmail.com>
2025-04-09 08:34:45 +0700

    fix(telescope): Switch to master to fix lsp picker issues

--- .config/nvim/lua/plugins/editor.lua
+++ .config/nvim/lua/plugins/editor.lua
@@ -72,7 +72,7 @@ return {
 	-- Add file search
 	{
 		"nvim-telescope/telescope.nvim",
-		branch = "0.1.x",
+		branch = "master",
 		dependencies = {
 			"nvim-lua/plenary.nvim",
 			"nvim-telescope/telescope-live-grep-args.nvim",
