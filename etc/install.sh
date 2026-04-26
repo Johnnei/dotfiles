4414b64a6cfb44b320fc8934fc433e9f954d0bf0
Johnnei <jtiesinga@gmail.com>
2026-04-26 19:53:17 +0700

    fix: Disable lan-local dns security

--- etc/install.sh
+++ etc/install.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
 
-set -euo pipefail
+set -u
+
+echo "Configuring systemd components"
+mkdir -p /etc/systemd/resolved.conf.d
+cp -v resolved.conf.d/* /etc/systemd/resolved.conf.d/
 
 echo "Linking systemd service overrides..."
 for unit in $(find systemd -mindepth 1 -type d)
 do
 	service=$(basename $unit)
-	ln -vsT $(pwd)/systemd/$service /etc/systemd/system/$service
+	mkdir -p /etc/systemd/system/$service
+	cp -v systemd/$service/* /etc/systemd/system/$service/
 done
