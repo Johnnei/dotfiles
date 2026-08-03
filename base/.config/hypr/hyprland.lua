5945ec6a7cdde821177f3ad17afdef52e689ba81
Johnnei <jtiesinga@gmail.com>
2026-08-03 21:32:34 +0700

    feat: Remove center gap

--- base/.config/hypr/hyprland.lua
+++ base/.config/hypr/hyprland.lua
@@ -92,7 +92,7 @@ hl.permission({
 -- Refer to https://wiki.hypr.land/Configuring/Basics/Variables/
 hl.config({
     general = {
-        gaps_in  = 2,
+        gaps_in  = 0,
         gaps_out = 0,
 
         border_size = 0,
